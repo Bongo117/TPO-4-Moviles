a31f0a2dcf9e30f2d9b6629d8134e2644156d98a
Federico Grippo <fedegrippo01@gmail.com>
2026-04-28 19:23:29 -0300

    Clean up

--- app/src/main/java/com/eskere/tpo4/MainActivity.java
+++ app/src/main/java/com/eskere/tpo4/MainActivity.java
@@ -31,12 +31,9 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-
         setSupportActionBar(binding.appBarMain.toolbar);
-        if (binding.appBarMain.fab != null) {
-            binding.appBarMain.fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).setAnchorView(R.id.fab).show());
-        }
+
+
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_content_main);
         assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
